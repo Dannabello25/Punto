9963639dcf00643a4b449dc9155131f60fcceb1e
danna bello <dmbellom@correo.udistrital.edu.co>
2022-11-23 16:56:13 -0500

    v.0.0.1

---
+++ Punto.py
@@ -0,0 +1,14 @@
+import math
+
+class Punto:
+
+    def __init__(self):
+        self.x=int
+        self.y=int
+        
+
+        
+    def calcular_distancia(self):
+        math.sqrt(math.pow(self.x-otroPunto.x,2)(self.y-otroPunto.y,2)
+
+    def otroPunto(self):
